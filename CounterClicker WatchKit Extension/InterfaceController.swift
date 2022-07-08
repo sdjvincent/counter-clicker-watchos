1c8b7953c940efcc3659d8fb68e7fb379d494c66
sdjvincent <vincent@avacadcam.com>
2022-07-08 08:30:22 +0100

    Initial Commit

---
+++ CounterClicker WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,26 @@
+//
+//  InterfaceController.swift
+//  CounterClicker WatchKit Extension
+//
+//  Created by Simon Vincent on 08/07/2022.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+    }
+
+}
