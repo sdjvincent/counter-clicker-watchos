e70103576b1a79dd1c9e39ba9c79351e5adcc002
sdjvincent <vincent@avacadcam.com>
2022-08-18 19:44:34 +0100

    Adding swipe gesture recognisers and replacing all button functions. Introducing swipes to change the amount users can increase or decrease by.

--- CounterClicker WatchKit Extension/InterfaceController.swift
+++ CounterClicker WatchKit Extension/InterfaceController.swift
@@ -15,10 +15,12 @@ class InterfaceController: WKInterfaceController {
     @IBOutlet weak var counterDisplay: WKInterfaceButton!
     
     var totalClicks: Int = 0
+    var plusAmount: Int = 1
     var player = AVAudioPlayer()
 
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
+        try! AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.ambient)
     }
     
     override func willActivate() {
@@ -30,24 +32,35 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
     }
     
-    @IBAction func clickerClicked() {
-        totalClicks = totalClicks + 1
+    @IBAction func increasePlusAmount(_ sender: Any) {
+        plusAmount = plusAmount + 1
+        counterDisplay.setTitle("+\(plusAmount)")
+    }
+    
+    @IBAction func decreasePlusAmount(_ sender: Any) {
+        plusAmount = plusAmount - 1
+        counterDisplay.setTitle("-\(plusAmount)")
+    }
+    
+    @IBAction func tapToPlus() {
+        totalClicks = totalClicks + plusAmount
         updateUI()
         playSound(soundName: "click")
     }
     
-    @IBAction func ClickerClickedMinus() {
-        totalClicks = totalClicks - 1
+    @IBAction func swipeDownMinus(_ sender: Any) {
+        totalClicks = totalClicks - plusAmount
         updateUI()
         playSound(soundName: "click")
     }
     
-    @IBAction func resetPressed() {
+    @IBAction func longPressReset(_ sender: Any) {
         totalClicks = 0
+        plusAmount = 1
         updateUI()
         playSound(soundName: "reset-click")
     }
-    
+ 
     func updateUI() {
     
         self.counterDisplay.setTitle("\(self.totalClicks)")
